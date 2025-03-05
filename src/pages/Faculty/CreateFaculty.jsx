a0b89ce21e81cb3426b419a7e80fd7af711ff785
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-03-05 13:18:43 +0500

    faculty department

---
+++ src/pages/Faculty/CreateFaculty.jsx
@@ -0,0 +1,80 @@
+import React, {useEffect} from "react";
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
+import {useMutation} from "@tanstack/react-query";
+import {CreateFacultyApi} from "../Api/FacultyApi.jsx";
+
+function CreateFaculty() {
+    const navigate = useNavigate();
+    const facultyMutation = useMutation({
+        mutationKey: ["create-faculty"],
+        mutationFn: CreateFacultyApi,
+        onSuccess: (data) => {
+            toast.success(data.message || "Fakultet muvaffaqiyatli yaratildi");
+        },
+        onError: (error) => {
+            toast.error(error.message || "Xatolik yuz berdi");
+        },
+    });
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().required("!!! To'ldirish shart"),
+        }),
+        onSubmit: (values) => {
+            // setFormData(values);
+            const facultyDate = {
+                name: values.name,
+            };
+            facultyMutation.mutate(facultyDate);
+        },
+    });
+
+    const isSuccess = facultyMutation.isSuccess;
+    useEffect(() => {
+        if (isSuccess) {
+            navigate("/list-faculty");
+        }
+    }, [navigate, isSuccess]);
+
+    return (<div className="space-y-6">
+        <h2 className="text-2xl font-bold text-gray-800">Fakultet qo'shish</h2>
+
+
+        <div className="bg-white rounded-lg shadow">
+            <div className="p-4 border-b bg-gray-50  ">
+                <form
+                    onSubmit={formik.handleSubmit}
+                    className="grid grid-cols-1 gap-3"
+                >
+                    <div className="w-full">
+                        <label htmlFor="name"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Fakultet nomi
+                        </label>
+                        <input type="text"
+                               id="name"
+                               name="name"
+                               {...formik.getFieldProps("name")}
+                               className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+
+                    </div>
+
+                    <button type="submit"
+                            className="focus:outline-none w-full text-white bg-[#3697A5] hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                        Qo'shish
+                    </button>
+                </form>
+            </div>
+
+
+        </div>
+
+    </div>);
+}
+
+export default CreateFaculty;
---
+++ src/pages/Faculty/ListFaculty.jsx
@@ -0,0 +1,125 @@
+import React, {useState} from "react";
+import {useQuery, useMutation} from "@tanstack/react-query";
+
+import toast from "react-hot-toast";
+import {Link} from "react-router-dom";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import {GetAllFaculty, DeleteFaculty} from "../Api/FacultyApi.jsx";
+
+function CategoryList() {
+    const [isModalOpen, setIsModalOpen] = useState(null);
+    const {isError, isSuccess, isLoading, data, error, refetch} = useQuery({
+        queryKey: ["list-faculty"],
+        queryFn: GetAllFaculty,
+    });
+
+    const facultyMutation = useMutation({
+        mutationKey: ["faculty-delete"],
+        mutationFn: DeleteFaculty,
+        onSuccess: (data) => {
+            toast.success(data.message || "Fakultet muvaffaqiyatli o'chirildi");
+            setIsModalOpen(null);
+        },
+        onError: (error) => {
+            toast.error(error.message || "Xatolik yuz berdi");
+        },
+    });
+
+
+    const handleDeleteClick = (facultyId) => {
+        setIsModalOpen(facultyId); // Modalni ochish
+    };
+
+    const deleteHandler = async (facultyId) => {
+
+            facultyMutation
+                .mutateAsync(facultyId)
+                .then(() => {
+                    refetch();
+                })
+                .catch((e) => console.log(e))
+
+    };
+    const cancelDelete = () => {
+        setIsModalOpen(null); // Modalni bekor qilish
+    };
+
+
+    return (
+        <div className="space-y-6">
+            <h2 className="text-2xl font-bold text-gray-800">Fakultetlar ro'yxati</h2>
+
+            <div className="bg-white rounded-lg shadow">
+                <div className="p-4">
+                    <table className="w-full">
+                        <thead>
+                        <tr className="text-left bg-gray-50">
+                            <th className="p-3 text-gray-600">Fakultet nomi</th>
+                            <th className="p-3 text-gray-600 flex justify-center ">Action</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data?.map((faculty, index) => {
+                            return (
+                                <tr className="border-t"
+                                key={faculty.id}>
+                                    <td className="p-3 ">{faculty?.name}</td>
+
+                                    <td className="p-3">
+                                        <div className="flex justify-center">
+                                            <Link
+                                                className=" flex items-center justify-start   pr-8"
+                                                // to={`/dashboard/edit-book/${book?.book_id}`}
+                                            >
+                                                <button><FaRegEdit className="text-2xl text-[#3697A5]"/></button>
+                                            </Link>
+                                            <button
+
+                                                className="flex items-center justify-start  "
+                                                // onClick={() => deleteHandler(faculty?.id)}
+                                                onClick={() => handleDeleteClick(faculty?.id)}
+                                            >
+                                                <MdDelete className="text-2xl text-red-600"/>
+                                            </button>
+                                            {isModalOpen === faculty?.id && (
+                                                <div className="fixed inset-0 flex items-center justify-center bg-gray-500/50">
+                                                    <div className="bg-white p-6 rounded-lg shadow-lg">
+                                                        <h2 className="text-lg font-semibold mb-4">
+                                                            Haqiqatan ham o‘chirmoqchimisiz?
+                                                        </h2>
+                                                        <p className="mb-6">
+                                                            <span className="text-red-600">{faculty?.name || 'Bu element'}</span> ni o‘chirishni tasdiqlaysizmi?
+                                                        </p>
+                                                        <div className="flex justify-end gap-4">
+                                                            <button
+                                                                className="px-4 py-2 bg-gray-300 rounded "
+                                                                onClick={cancelDelete}
+                                                            >
+                                                                Bekor qilish
+                                                            </button>
+                                                            <button
+                                                                className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+                                                                onClick={() => deleteHandler(faculty?.id)}
+                                                            >
+                                                                O‘chirish
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CategoryList;
